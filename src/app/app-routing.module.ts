f256a5b6a4607c41b5811d63ed0c208bc5ab2779
isaskou <isabel.skou@yahoo.fr>
2021-01-22 18:05:25 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './_components/Main/main.component';
+import { ClientComponent } from './_components/Pages/client/client.component';
 import { DashboardComponent } from './_components/Pages/dashboard/dashboard.component';
 import { FactureComponent } from './_components/Pages/facture/facture.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: '', redirectTo: 'main/dashboard', pathMatch: 'full' },
   {path:'main', redirectTo:'main/dashboard', pathMatch:'full'},
   {
     path: 'main', component: MainComponent, children:
       [
         { path: 'dashboard', component: DashboardComponent },
         { path: 'facture', component: FactureComponent },
+        {path: 'client', component:ClientComponent}
       ]
   },
   // {path:'dashboard', component:DcMenuComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './_components/Layout/navbar/navbar.component';
 import { SidebarComponent } from './_components/Layout/sidebar/sidebar.component';
@@ -9,13 +13,17 @@ import { DropdownComponent } from './_components/Layout/dropdown/dropdown.compon
 import { DashboardComponent } from './_components/Pages/dashboard/dashboard.component';
 import { CaisseComponent } from './_components/Pages/caisse/caisse.component';
 import { FactureComponent } from './_components/Pages/facture/facture.component';
-import { FcTitleComponent } from './_components/Pages/facture/fc-content/fc-title/fc-title.component';
+import { FcTitleComponent } from './_components/Pages/facture/fc-accueil/fc-title/fc-title.component';
 import { DcTitleComponent } from './_components/Pages/dashboard/dc-content/dc-title/dc-title.component';
 import { DcButtonComponent } from './_components/Pages/dashboard/dc-content/dc-button/dc-button.component';
 import { DcTableComponent } from './_components/Pages/dashboard/dc-content/dc-table/dc-table.component';
-import { FcButtonComponent } from './_components/Pages/facture/fc-content/fc-button/fc-button.component';
-import { FcGetAllComponent } from './_components/Pages/facture/fc-content/fc-get-all/fc-get-all.component';
+import { FcButtonComponent } from './_components/Pages/facture/fc-accueil/fc-button/fc-button.component';
+import { FcTableComponent } from './_components/Pages/facture/fc-accueil/fc-table/fc-table.component';
 import { MainComponent } from './_components/Main/main.component';
+import { ClientComponent } from './_components/Pages/client/client.component';
+import { ClAccueilTitleComponent } from './_components/Pages/client/cl-accueil/cl-accueil-title/cl-accueil-title.component';
+import { ClAccueilButtonComponent } from './_components/Pages/client/cl-accueil/cl-accueil-button/cl-accueil-button.component';
+import { ClAccueilTableComponent } from './_components/Pages/client/cl-accueil/cl-accueil-table/cl-accueil-table.component';
 
 @NgModule({
   declarations: [
@@ -31,12 +39,20 @@ import { MainComponent } from './_components/Main/main.component';
     DcButtonComponent,
     DcTableComponent,
     FcButtonComponent,
-    FcGetAllComponent,
+    FcTableComponent,
     MainComponent,
+    ClientComponent,
+    ClAccueilTitleComponent,
+    ClAccueilButtonComponent,
+    ClAccueilTableComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
